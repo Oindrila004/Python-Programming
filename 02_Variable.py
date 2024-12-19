3f83974b94a318c7f635fa4c721515f45086d5c2
Oindrila Majumder <oindrilamajumder3@gmail.com>
2024-12-19 20:54:59 +0530

    Update

---
+++ 02_Variable.py
@@ -0,0 +1,17 @@
+name = "Oindrila"#Need to use "" to write a string
+age = 20
+year = 2024
+
+print("My name is :",name)
+print("My age is :",age)
+print("The year is :",year)
+#identifying the data types
+print(type(name))
+print(type(age))
+print(type(year))
+
+#Boolean Data type
+old = False #First letter of boolean value should be in capital letter
+a = None
+print(type(old))
+print(type(a))
---
+++ 03_Operators.py
@@ -0,0 +1,10 @@
+#Arithmetic Operators
+num1 = 40
+num2 = 10
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2) #Modulo Operator(remainder)
+print(num1 ** num2) #num1^num2 (To calculate power)
