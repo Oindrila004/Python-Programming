f130301fabc0b6ad70cd99ad0c99e7bae3b501c9
Oindrila Majumder <oindrilamajumder3@gmail.com>
2024-12-19 21:12:33 +0530

    Update

--- 03_Operators.py
+++ 03_Operators.py
@@ -8,3 +8,29 @@ print(num1 * num2)
 print(num1 / num2)
 print(num1 % num2) #Modulo Operator(remainder)
 print(num1 ** num2) #num1^num2 (To calculate power)
+
+#Relational Operator
+a = 50
+b = 20
+print(a == b) #False
+print(a != b) #True
+print(a >= b) #True
+print(a > b)
+print(a <= b) #False
+print(a < b)
+
+#Assignment Operator
+num = 15
+num += 10 #num = num + 10
+print(num)
+
+#Logical Operator
+a = 50
+b = 30
+print(not False)
+print(not ( a > b))
+
+val1 = True
+val2 = True
+print("and operator:" , val1 and val2) #only returns true when both the values are true, otherwise it returns false
+print("or operator:" , val1 or val2) #returns true if any one of the value is true, not mandatory is both the value is true
